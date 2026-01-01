cfdc4146ed03e6aa208bc28383c80e74271f5319
DJTJ9 <114483273+DJTJ9@users.noreply.github.com>
2026-01-01 23:22:57 +0100

    StateMachineBehaviours for Swaggy Snapshots dance moves

---
+++ Assets/DevTjark/Scripts/Scriptable Objects/DanceMoveTriggersSO.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dance Move Triggers", menuName = "Scriptable Objects/Swaggy Snapshots/Dance Move Triggers", order = 1)]
+public class DanceMoveTriggersSO : SerializedScriptableObject
+{
+    public Dictionary<DanceMoveTriggers, string> DanceMoveTriggersDictionary = new Dictionary<DanceMoveTriggers, string>();
+
+    private DanceMoveTriggers m_lastKey;
+    private bool m_hasLastKey; 
+    
+    public string GetNextDanceMove()
+    {
+        if (DanceMoveTriggersDictionary == null || DanceMoveTriggersDictionary.Count == 0)
+            throw new System.InvalidOperationException("Dictionary is empty.");
+        
+        if (DanceMoveTriggersDictionary.Count == 1)
+            return DanceMoveTriggersDictionary.Values.First();
+
+        DanceMoveTriggers newKey;
+
+        do
+        {
+            int index = Random.Range(0, DanceMoveTriggersDictionary.Count);
+            newKey = DanceMoveTriggersDictionary.ElementAt(index).Key;
+        }
+        while (m_hasLastKey && newKey == m_lastKey);
+        
+        m_lastKey = newKey;
+        m_hasLastKey = true;
+        
+        return DanceMoveTriggersDictionary[newKey];
+    }
+}
---
+++ Assets/DevTjark/Scripts/Swaggy Snapshots/Dance states/DanceMove6.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DanceMove6 : StateMachineBehaviour
+{
+    [SerializeField] private float m_duration = 3f;
+    
+    [SerializeField] private DanceMoveTriggersSO danceMoveTriggersSO;
+
+    private float timer;
+    private bool hasTriggered;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer = 0;
+        hasTriggered = false;
+    }
+    
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (hasTriggered) return;
+        
+        timer += Time.deltaTime;
+        
+        if (timer >= m_duration)
+        {
+            hasTriggered = true;
+            animator.SetTrigger(danceMoveTriggersSO.GetNextDanceMove());
+        }
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer = 0;
+    }
+}
