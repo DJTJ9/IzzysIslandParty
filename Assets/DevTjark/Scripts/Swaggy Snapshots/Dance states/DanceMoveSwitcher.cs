2677ae348ca45ccb69f1b58a1045e118846f7fd0
DJTJ9 <114483273+DJTJ9@users.noreply.github.com>
2026-01-05 18:56:18 +0100

    StateMachineBehaviour replaced by DanceMoveManager, OnDanceMoveChanged event added, Animations fixed, Flashlight adjusted

--- Assets/DevTjark/Scripts/Swaggy Snapshots/Dance states/DanceMoveSwitcher.cs
+++ Assets/DevTjark/Scripts/Swaggy Snapshots/Dance states/DanceMoveSwitcher.cs
@@ -2,34 +2,44 @@
 
 public class DanceMoveSwitcher : StateMachineBehaviour
 {
-    [SerializeField] private float m_duration = 3f;
-    
-    [SerializeField] private DanceMoveTriggersSO danceMoveTriggersSO;
+    // [Header("Settings")]
+    // [SerializeField]
+    // private float m_duration = 3f;
 
-    private float timer;
-    private bool hasTriggered;
+    [SerializeField]
+    private DanceMoveTriggersSO danceMoveTriggersSO;
+
+    [SerializeField]
+    private GameEvent onDanceMoveChanged;
+
+    private Animator m_animator;
+    // private float timer;
+    // private bool hasTriggered;
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        timer = 0;
-        hasTriggered = false;
-    }
-    
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        if (hasTriggered) return;
-        
-        timer += Time.deltaTime;
-        
-        if (timer >= m_duration)
-        {
-            hasTriggered = true;
-            animator.SetTrigger(danceMoveTriggersSO.GetNextDanceMove());
-        }
+        m_animator = animator;
+        // timer = 0;
+        // hasTriggered = false;
     }
 
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // {
+    //     if (hasTriggered) return;
+    //     
+    //     timer += Time.deltaTime;
+    //     
+    //     if (timer >= m_duration) OnDanceMoveChange();
+    // }
+
+    public void OnDanceMoveChange()
     {
-        timer = 0;
+        // hasTriggered = true;
+        m_animator.SetTrigger(danceMoveTriggersSO.GetNextDanceMove());
     }
+
+    // public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // {
+    //     timer = 0;
+    // }
 }
