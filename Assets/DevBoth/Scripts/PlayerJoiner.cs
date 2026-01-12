4bcf8bc025e51c61fd382e3c47affe8ac41dde45
DJTJ9 <114483273+DJTJ9@users.noreply.github.com>
2026-01-12 19:52:07 +0100

    First multiplayer logic, Minigolf player prefab

---
+++ Assets/DevBoth/Scripts/PlayerJoiner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerJoiner : MonoBehaviour
+{
+    public Transform SpawnPoint1, Spawnpoint2, Spawnpoint3;
+    public GameObject Player1, Player2, Player3;
+
+    private void Awake()
+    {
+        Instantiate(Player1, SpawnPoint1.position, SpawnPoint1.rotation);
+        Instantiate(Player2, Spawnpoint2.position, Spawnpoint2.rotation);
+        // Instantiate(Player3, Spawnpoint3.position, Spawnpoint3.rotation);
+    }
+}
