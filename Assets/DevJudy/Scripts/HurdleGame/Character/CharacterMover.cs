fa3b8086abc21e2661aaceee302762a0ebd12cd8
J. Albrecht <juju.dith@web.de>
2026-01-20 23:04:47 +0100

    tried fixing bounciness (did not work)

--- Assets/DevJudy/Scripts/HurdleGame/Character/CharacterMover.cs
+++ Assets/DevJudy/Scripts/HurdleGame/Character/CharacterMover.cs
@@ -9,6 +9,7 @@ namespace HurdleGame
 
         [Header("Movement variables: ")]
         [SerializeField] private FloatReference moveSpeed;
+        [SerializeField] private FloatReference velocity;
         private float moveDirMultiplier = 100f;
         private Vector3 colliderMoveDir;
 
@@ -25,7 +26,11 @@ namespace HurdleGame
 
         private void FixedUpdate()
         {
-            rb.linearVelocity = new Vector3((moveDirMultiplier * moveSpeed.Value) * Time.deltaTime, rb.linearVelocity.y, 0f);
+            // !! This shit keeps bopping up and down
+            rb.linearVelocity = new Vector3((moveDirMultiplier * moveSpeed.Value) * Time.deltaTime, 0f, rb.linearVelocity.z);
+            
+            
+            //Ditzelgames.PhysicsHelper.ApplyForceToReachVelocity(rb, new Vector3(velocity.Value, 0f, 0f), (moveSpeed.Value * moveDirMultiplier) * Time.deltaTime);
             
             prevYVelocity = rb.linearVelocity.y;
         }
