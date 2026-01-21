a678ef68be8db647d588931d94c670006ec83b91
DJTJ9 <114483273+DJTJ9@users.noreply.github.com>
2026-01-21 14:37:17 +0100

    multiplayer started

--- Assets/DevTjark/Scripts/Scriptable Objects/BowlingBallSO.cs
+++ Assets/DevTjark/Scripts/Scriptable Objects/BowlingBallSO.cs
@@ -1,11 +1,13 @@
 ﻿using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "Bowling Ball", menuName = "Scriptable Objects/Bowling Ball", order = 1)]
 public class BowlingBallSO : SerializedScriptableObject
 {
     public GameObject PlayerPrefab;
+    public PlayerInput PlayerInput;
     public BallType CurrentBallType;
     public GameScoreSO PlayerScore;
     public Vector3 SpawnPoint;
