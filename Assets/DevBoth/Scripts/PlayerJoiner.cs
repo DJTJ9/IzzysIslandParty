1a4df2c30e4b0acf37792660367a66e902fbc506
DJTJ9 <114483273+DJTJ9@users.noreply.github.com>
2026-01-29 17:46:00 +0100

    npc join dynamicly, npc prefabs and collection

--- Assets/DevBoth/Scripts/PlayerJoiner.cs
+++ Assets/DevBoth/Scripts/PlayerJoiner.cs
@@ -7,17 +7,21 @@ using UnityEngine.Serialization;
 
 public class PlayerJoiner : MonoBehaviour
 {
-    public SO_PlayerCollection soPlayerCollection;
+    [FoldoutGroup("Bowling Battle")]
+    public SO_PlayerCollection playerCollectionSO;
+    [SerializeField] private SO_PlayerCollection npcCollection;
 
+    [Header("Player Inputs")]
     [SerializeField] private SO_PlayerInputs playerInputs;
     // private List<PlayerInput> players = new List<PlayerInput>();
     
     private int m_playerIndex = 0;
+    private int m_npcIndex = 0;
 
     // private void Awake()
     // {
     //     // playerInputManager = FindFirstObjectByType<PlayerInputManager>();
-    //     // playerInputManager.onPlayerJoined += PlayerJoined;
+    //     // playerInputManager.onPlayerJoined += PlayerJoinedBB;
     //     SpawnPlayer(0);
     //     // SpawnPlayer(1);
     //     // Instantiate(Player2, Spawnpoint2.position, Spawnpoint2.rotation);
@@ -31,17 +35,34 @@ public class PlayerJoiner : MonoBehaviour
         // SpawnPlayer(3);
     }
 
-    public void PlayerJoined(PlayerInput _playerInput)
+    public void PlayerJoinedBB(PlayerInput _playerInput)
     {
-        _playerInput.gameObject.transform.position = soPlayerCollection.Players[m_playerIndex].SpawnPoint;
-        soPlayerCollection.Players[m_playerIndex].InitializePlayer(_playerInput.gameObject, soPlayerCollection.Players[m_playerIndex], m_playerIndex);
+        // _playerInput.gameObject.GetComponent<PlayerControllerBowlingBattle>().BindPlayerSO(playerCollectionSO.Players[m_playerIndex]);
+
+        if (_playerInput.gameObject.TryGetComponent(out NPC_BowlingBattle npc))
+        {
+            _playerInput.gameObject.transform.position = npcCollection.Players[m_npcIndex].SpawnPoint;
+            ++m_npcIndex;
+            return;
+        }
+        
+        _playerInput.gameObject.transform.position = playerCollectionSO.Players[m_playerIndex].SpawnPoint;
+        playerCollectionSO.Players[m_playerIndex].InitializePlayer(_playerInput.gameObject, playerCollectionSO.Players[m_playerIndex], m_playerIndex);
         ++m_playerIndex;
         // AddPlayer(_playerInput);
     }
 
+    public void JoinNPCsBB()
+    {
+        for (var i = m_playerIndex - 1; i < npcCollection.Players.Count; i++)
+        {
+            Instantiate(npcCollection.Players[i].PlayerPrefab, npcCollection.Players[i].SpawnPoint, Quaternion.identity);
+        }
+    }
+
     public void PlayerJoinedJJ(PlayerInput _playerInput)
     {
-        _playerInput.gameObject.transform.position = soPlayerCollection.Players[m_playerIndex].SpawnPoint;
+        _playerInput.gameObject.transform.position = playerCollectionSO.Players[m_playerIndex].SpawnPoint;
         ++m_playerIndex;
     }
     
@@ -57,6 +78,6 @@ public class PlayerJoiner : MonoBehaviour
     [Button]
     public void SpawnPlayer(int _playerIndex)
     {
-        Instantiate(soPlayerCollection.Players[_playerIndex].PlayerPrefab, soPlayerCollection.Players[_playerIndex].SpawnPoint, Quaternion.identity);
+        Instantiate(playerCollectionSO.Players[_playerIndex].PlayerPrefab, playerCollectionSO.Players[_playerIndex].SpawnPoint, Quaternion.identity);
     }
 }
