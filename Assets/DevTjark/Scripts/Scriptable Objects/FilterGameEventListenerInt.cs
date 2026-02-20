9c2bc5fb1fc10d400e40daaca635900c1be36db4
DJTJ9 <114483273+DJTJ9@users.noreply.github.com>
2026-02-20 11:23:40 +0100

    generic game events, hot reload updated

---
+++ Assets/DevTjark/Scripts/Scriptable Objects/FilterGameEventListenerInt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FilterGameEventListenerInt : GameEventListenerInt
+{
+    public int TriggerValue;
+
+    public override void OnEventRaised(int value)
+    {
+        // If value not expected value
+        if (value != TriggerValue) return;
+    }
+}
