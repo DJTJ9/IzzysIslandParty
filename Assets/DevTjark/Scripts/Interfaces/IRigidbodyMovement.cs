1e4bd1439b9006934e417a1bed6787d3dd672d4a
DJTJ9 <114483273+DJTJ9@users.noreply.github.com>
2026-02-21 17:37:23 +0100

    impulse nearly fixed, titan traps started

--- Assets/DevTjark/Scripts/Interfaces/IRigidbodyMovement.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public interface IRigidbodyMovement
-{
-    public float GetCurrentVelocity();
-}
--- Assets/DevTjark/Scripts/Minigolf Mayhem/PlayerControllerMinigolfMayhem.cs
+++ Assets/DevTjark/Scripts/Minigolf Mayhem/PlayerControllerMinigolfMayhem.cs
@@ -27,58 +27,10 @@ public class PlayerControllerMinigolfMayhem : Controller
         ShootCount = 0;
     }
 
-
-    /// <summary>
-    /// Sets cursor lock mode on left click to locked and on escape to none.
-    /// Gets move direction from input and moves rigidbody into this direction.
-    /// Rotates the rigidbody horizontally if cursor lock mode is locked.
-    /// </summary>
-    private void Update()
-    {
-        // if (Mouse.current.rightButton.wasPressedThisFrame) Cursor.lockState = CursorLockMode.Locked;
-        // if (Mouse.current.rightButton.wasPressedThisFrame) Cursor.lockState = CursorLockMode.None;
-
-        // var moveDirection = GetMoveDirectionFromInput();
-        // rigidbodyMovement.Move(moveDirection);
-
-        // if (Cursor.lockState == CursorLockMode.Locked) {
-        // var rotation = GetRotationFromInput();
-        // rigidbodyMovement.RotateHorizontal(rotation.x * lookSensitivity);
-        // }
-    }
-
-    /// <summary>
-    /// Maps the input actions
-    /// Subcribes methods to their matching input actions
-    /// </summary>
-    // private void MapInputActions()
-    // {
-    //     //     moveInputAction = playerInput.actions["Move"];
-    //     //     moveInputAction.started += OnMoveInput;
-    //     //
-    //     //     jumpInputAction = playerInput.actions["Jump"];
-    //     //     jumpInputAction.started += OnJumpInput;
-    //     //
-    //     //     lookInputAction = playerInput.actions["Look"];
-    //     //
-    //     //     shootInputAction = playerInput.actions["LeftMouse"];
-    //     //     shootInputAction.started += OnShootInput;
-    //     //
-    //     //     pauseInputAction = playerInput.actions["Pause"];
-    //     //     pauseInputAction.started += OnPauseInput;
-    //     //     
-    //     //     unpauseInputAction = playerInput.actions["Unpause"];
-    //     //     unpauseInputAction.started += OnUnpauseInput;
-    // }
     public override void SwitchToPlayerInputMap()
     {
         playerInput.SwitchCurrentActionMap("MinigolfMayhem");
     }
-
-    // public void SwitchToUIInputMap()
-    // {
-    //     playerInput.SwitchCurrentActionMap("UI");
-    // }
 
     public void OnPauseInput(InputAction.CallbackContext _context)
     {
